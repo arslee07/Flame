ce243eaaf730ccd930044d210d532038b6dad687
torvalds <volodymyr.germany.2020@gmail.com>
2021-04-04 12:20:06 +0200

    improvement: Improved a 200 message.

--- api/routes.js
+++ api/routes.js
@@ -4,7 +4,7 @@ module.exports = [
         path: '/',
         handler: (req, res) => {
             return res.send(
-                { statusCode: 200, message: 'API works successfull. Check documentation for more info.' }
+                { statusCode: 200, message: 'API works!. Check documentation for more info.' }
             );
         }
     },
